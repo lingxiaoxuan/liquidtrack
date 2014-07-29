0b26596cb3b29136557ceb7ee9f1e07d14877454
jizg <hoogee@gmail.com>
2014-07-30 07:53:32 +0800

    fix version problems

--- app/src/com/ibm/bluemix/samples/DeleteTrackServlet.java
+++ app/src/com/ibm/bluemix/samples/DeleteTrackServlet.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.ibm.bluemix.samples;
 
 import java.io.IOException;
@@ -40,46 +39,4 @@ public class DeleteTrackServlet extends HttpServlet {
 		}
 	}
 }
-=======
-package com.ibm.bluemix.samples;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-@SuppressWarnings("serial")
-public class DeleteTrackServlet extends HttpServlet {
-	private PostgreSQLClient db = new PostgreSQLClient();
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("DeleteTrackServlet post");
-		
-		try {
-			String notesIDList = request.getParameter("NotesIDList");
-			System.out.printf("notesIDList:%s", notesIDList);
-
-			if (notesIDList != null && !("").equals(notesIDList)) {
-				String[] strList = notesIDList.split(",");
-				String notesID = "";
-				String liquidID = "";
-				int rows = 0;
-				for (String str : strList) {
-					if (!("").equals(str)) {
-						notesID = str.split("_")[0];
-						liquidID = str.split("_")[1];
-						if (liquidID != null && !"".equals(liquidID)) {
-							rows += db.deleteTrack(notesID, liquidID);
-						}
-					}
-				}
-				System.out.printf("%d", rows);
-			}
-		} catch (Exception e) {
-			e.printStackTrace(System.err);
-		}
-	}
-}
->>>>>>> refs/remotes/origin/master
